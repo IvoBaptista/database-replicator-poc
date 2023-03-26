93cd45118881f618f4dd9c8e0944dceafbeac54b
IvoBap <ivobaptista1@gmail.com>
2023-03-26 14:51:18 +0100

    Add replica db Product entity and repository

---
+++ src/main/java/com/poc/db/replicator/replica/ProductReplica.java
@@ -0,0 +1,28 @@
+package com.test.db.replicator.replica;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "PRODUCT")
+public class ProductReplica {
+    private String warehouse;
+    @Id
+    @Column(name = "BAR_CODE")
+    private String barCode;
+    private String designation;
+    private String category;
+    @Column(name = "SUB_CATEGORY")
+    private String subCategory;
+    private double price;
+    private int stock;
+    private String refs;
+}
---
+++ src/main/java/com/poc/db/replicator/replica/ProductReplicaRepository.java
@@ -0,0 +1,10 @@
+package com.test.db.replicator.replica;
+
+import org.springframework.data.repository.CrudRepository;
+
+
+import java.util.List;
+
+
+public interface ProductReplicaRepository extends CrudRepository<ProductReplica, Long> {
+}
