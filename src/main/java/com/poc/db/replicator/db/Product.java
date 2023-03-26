f1f740c7d9814e8fde33617c6c9dbeb2392a2c39
IvoBap <ivobaptista1@gmail.com>
2023-03-26 14:50:42 +0100

    Add original db product entity and repository

---
+++ src/main/java/com/poc/db/replicator/db/Product.java
@@ -0,0 +1,22 @@
+package com.test.db.replicator.db;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class Product {
+    private String warehouse;
+    @Id
+    @Column(name = "BAR_CODE")
+    private String barCode;
+    private String designation;
+    private String category;
+    @Column(name = "SUB_CATEGORY")
+    private String subCategory;
+    private double price;
+    private int stock;
+    private String refs;
+}
---
+++ src/main/java/com/poc/db/replicator/db/ProductRepository.java
@@ -0,0 +1,10 @@
+package com.test.db.replicator.db;
+
+import org.springframework.data.repository.Repository;
+
+import java.util.List;
+
+
+public interface ProductRepository extends Repository<Product, Long> {
+    List<Product> findAll();
+}
